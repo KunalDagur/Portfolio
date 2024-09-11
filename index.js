c7f4ea00e3ea7101832c4b7e3de3fbdbbad7afe1
KunalDagur <kdagur34@gmail.com>
2024-09-11 13:12:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings:['Frontend Developer', 'Web Developer', 'Java Developer', 'Designer'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 500,
+    loop:true
+});
+
