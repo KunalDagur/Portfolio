da3342940010d8f694cfc0d1d34be618721cbf45
KunalDagur <kdagur34@gmail.com>
2024-10-11 11:01:57 +0530

    Updated soical media links

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var typed = new Typed(".text", {
-    strings:['Frontend Developer', 'Web Developer', 'Java Developer', 'Designer'],
+    strings: ['Frontend Developer', 'Web Developer', 'Java Developer', 'Designer'],
     typeSpeed: 50,
     backSpeed: 50,
     backDelay: 500,
-    loop:true
+    loop: true
 });
-
